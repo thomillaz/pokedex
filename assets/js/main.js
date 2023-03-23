cfc6c21d5f7e3fe20a8564af6d6dc8f048248354
thomillaz <kamomillathomaz@gmail.com>
2023-03-22 22:20:50 -0300

    estilização

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then(function (response){
+        response.json().then(function (responseBody){
+            console.log(responseBody)
+        })
+    })
+    .catch(function (error){
+        console.errpr(error)
+    })
+    .finally(function (){
+        console.log("Requisição concluída!")
+    })
